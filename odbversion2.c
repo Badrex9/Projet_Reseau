b1b2b0c597e8feefb86c9ee70a1edce743520f0c
tahazz5 <130492908+tahazz5@users.noreply.github.com>
2023-12-04 13:48:54 +0000

    change odb

--- odbversion2.c
+++ odbversion2.c
@@ -9,22 +9,27 @@
 #include <arpa/inet.h>
 #include <fcntl.h>
 #define BUFFER_LEN 200
+#define BUFFER_SHARE_LEN 2000
 enum Etat {
     reel,
     virtuelle,
     none
 };
 
-struct request{
-	long id; 
-	int lentgh;
-};
 struct Buff_vir
 {
+    char type;
+    size_t size;
 	char ip[15];
 	long id;
 	int offset;
 	int lentgh;
+};
+struct Buff_reel
+{
+    char type;
+    size_t size;
+    char preload[BUFFER_LEN];
 };
 enm Etat socket_ETat =  none;
 char buffer_share[BUFFER_SHARE_LEN];
@@ -46,40 +51,35 @@ char type_preload(const void *buffer){
 
  ssize_t write(int fd, const void *buf, size_t count){
     original_write_t original_write = (original_write_t)dlsym(RTLD_NEXT, "write");
-     char* buffer = malloc(BUFFER_LEN*sizeof(char));
     if(type_preload(buf)=='R'){
-        *buffer ='R';
-        *(buffer+1) = count ;
-        *(buffer+sizeof(size_t)/sizeof(char)) = buff;
+        char* buffer = malloc(sizeof(struct Buff_reel));
+        buffer->type ='R';
+        buffer->size = count ;
+        buffer->preload= *buff;
         enm Etat socket_ETat = reel ;
         return original_write(fd,buffer, count+1);
     }
     else{
-        *buffer ='V';
+        char* buffer = malloc(sizeof(struct  Buff_vir));
+        buffer->type ='v'
         *(buffer+1) = count ;
-        struct sockaddr_in addr;
-        socklen_t addrLen = sizeof(addr);
-
+        buffer->size = count ;
         if (getpeername(sockfd, (struct sockaddr*)&addr, &addrLen) == -1) {
             perror("Erreur lors de l'extraction de l'adresse et du port");
             exit(1);
         }
         char ip[15];
         inet_ntop(AF_INET, &(addr.sin_addr), ip, 15);
-        int port = ntohs(addr.sin_port);
-        struct Buff_vir* buffer_virtuel = malloc(sizeof(struct Buff_vir));
-	    bzero(buffer_virtuel, sizeof(struct Buff_vir));
+        int port = ntohs(addr.sin_port);;
 	    //Initialisation de l'offset du buffer_share
-	    buffer_virtuel->offset = 0; 
-	    buffer_virtuel->lentgh = strlen(buf);
-        buffer_virtuel->ip =ip ;
-        buffer_virtuel->port = port ;
-        buffer_virtuel->id = identifiant ;   
+	    buffer->offset = 0; 
+        buffer->ip =ip ;
+        buffer->port = port ;
+        buffer->id = identifiant ;   
         identifiant =+ buffer_virtuel->lentgh ;
-        *(buffer+sizeof(size_t)/sizeof(char))=*(buffer_virtuel)
         strcpy(buffer_share,buff);
         enm Etat socket_ETat = virtuelle ;
-        return original_write(fd,buffer,sizeof(struct Buff_vir)+sizeof(char)+sizeof(ssize_t));
+        return original_write(fd,buffer,sizeof(struct Buff_vir));
     }
    
     
@@ -87,14 +87,16 @@ char type_preload(const void *buffer){
  }   
  ssize_t read(int fd, void *buf, size_t count){
     original_read_t original_read = (original_read_t)dlsym(RTLD_NEXT, "read");
-    if(socket_ETat=reel){
-      return original_read(fd, buff,count);
+    ssize_t result;
+    if(socket_ETat==reel){
+      result= original_read(fd, buf->preload,count);
     }
-    else{
-      buffer_share[identifiant];
-      original_read(fd, buff,count);
-      
-
+    else if(socket_ETat==virtuelle){
+      result= original_read(fd, &buffer_share[buf->identifiant],count);
     }
-     
+    else {
+        return;
+    }
+    socket_ETat=none ;
+    return result ;
  }
